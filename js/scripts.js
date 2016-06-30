7a5973969d516b9ce24731a05bfdde4f9b5058d8
J. Ryan Conklin and Scott Scholz <jryanconklin@gmail.com>
2016-06-29 19:54:58 -0700

    Add the forEach loop for address append to the contact click to display address info in js/scripts.js.

--- js/scripts.js
+++ js/scripts.js
@@ -15,19 +15,9 @@ Contact.prototype.fullName = function() {
   return this.firstName + " " + this.lastName;
 }
 
-$(".new-address").each(function() {
-  var inputtedStreet = $(this).find("input.new-street").val();
-  var inputtedCity = $(this).find("input.new-city").val();
-  var inputtedState = $(this).find("input.new-state").val();
-  var newAddress = new Address(inputtedStreet, inputtedCity, inputtedState);
-  newContact.addresses.push(newAddress);
-});
-
-
-
-
 // user interface logic
 $(document).ready(function() {
+
   $("#add-address").click(function() {
     $("#new-addresses").append('<div class="new-address">' +
                                   '<div class="form-group">' +
@@ -53,18 +43,32 @@ $(document).ready(function() {
 
     var newContact = new Contact(inputtedFirstName, inputtedLastName);
 
+    $(".new-address").each(function() {
+      var inputtedStreet = $(this).find("input.new-street").val();
+      var inputtedCity = $(this).find("input.new-city").val();
+      var inputtedState = $(this).find("input.new-state").val();
+      var newAddress = new Address(inputtedStreet, inputtedCity, inputtedState);
+      newContact.addresses.push(newAddress);
+    });
 
     $("ul#contacts").append("<li><span class='contact'>" + newContact.fullName() + "</span></li>");
-
-    $("input#new-first-name").val("");
-    $("input#new-last-name").val("");
 
     $(".contact").last().click(function() {
       $("#show-contact").show();
       $("#show-contact h2").text(newContact.firstName);
       $(".first-name").text(newContact.firstName);
       $(".last-name").text(newContact.lastName);
+      $("ul#addresses").text("");
+      newContact.addresses.forEach(function(address) {
+        $("ul#addresses").append("<li>" + address.street + ", " + address.city + " " + address.state + "</li>");
+      });
     });
+
+
+    $("input#new-first-name").val("");
+    $("input#new-last-name").val("");
+
+
 
   }); // End form function
 
